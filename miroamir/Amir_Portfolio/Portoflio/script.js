04235616a56c41c249b7642e0288ee0a415d2d4c
119443772+miroamir@users.noreply.github.com <miroamir422@gmail.com>
2024-08-08 21:35:25 +0100

    added javascript

--- miroamir/Amir_Portfolio/Portoflio/script.js
+++ miroamir/Amir_Portfolio/Portoflio/script.js
@@ -4,3 +4,36 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+
+
+const sections = document.querySelectorAll('section');
+
+const options = {
+    root: null,
+    threshold: 0.1,
+    rootMargin: '0px',
+};
+
+const observer = new IntersectionObserver((entries, observer) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('fade-in');
+            observer.unobserve(entry.target);
+        }
+    });
+}, options);
+
+sections.forEach(section => {
+    observer.observe(section);
+});
+
+
+// script.js
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+          behavior: 'smooth'
+      });
+  });
+});
